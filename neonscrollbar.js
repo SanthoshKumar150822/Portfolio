8c838b7df4a5dfd5d097cae414828192df9f645f
SanthoshKumar150822 <santhoshkumar150822@gmail.com>
2026-09-19 14:23:15 +0530

    Wipe all files from repository

--- neonscrollbar.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-  $(window).scroll(function(){
-  var scroll = $(window).scrollTop(),
-  dh = $(document).height(),
-  wh = $(window).height();
-  scrollPerceent = (scroll/(dh-wh))*100;
-  $('#progressbar').css('height', scrollPercent + %);
-  })
-</script>
